dbb3d5108e3f5432f125a7a040246a5516164f9e
cameron perry <cameronperry@camerons-MacBook-Pro.local>
2023-09-04 18:54:28 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// home page
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// show all listings 
+
+Route::get('/{property_type}/{listing_type}/', function () {
+    return view('welcome');
+});
+
+
+// single listing
+Route::get('/listing/{slug}/{id}', function () {
+    return view('welcome');
+});
+
+// user login 
+
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+// user register
+
+Route::get('/register', function () {
+    return view('welcome');
+});
+
+// user saved listings 
+
+Route::get('/account/saved', function () {
+    return view('welcome');
+});
+
+// user show status 
+
+Route::get('/account/show-status', function () {
+    return view('welcome');
+});
+
+
